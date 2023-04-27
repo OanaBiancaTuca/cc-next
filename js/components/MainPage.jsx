f2773d349376b3c4e182e05a0f562d41cc9493ba
OanaBiancaTuca <tucaoana19@stud.ase.ro>
2023-04-27 18:52:32 +0300

    OPENAI Init

--- js/components/MainPage.jsx
+++ js/components/MainPage.jsx
@@ -12,54 +12,57 @@ export default function MainPage() {
 				.then(response => response.json())
 				.then(json => setRecords(json.data));
 		}
-		catch (error) {
-			console.log(error);
+		catch(e){
+			console.log(e);
 		}
 	}, []);
 
-	const deleteRecord = (event) => {
-		event.preventDefault();
-		const id = event.target.id;
-		try {
+	const deleteRecord = async (e) => {
+		e.preventDefault();
+
+		console.log(e.target.id);
+		const id = e.target.id;
+
+		try{
 			fetch(`/api/records?id=${id}`, {
 				method: 'DELETE',
 			})
 				.then(response => response.json())
 				.then(json => {
-						setRecords(records.filter(record => record._id !== id));
+					setRecords(records.filter(record => record._id !== id));
 				});
 		}
-		catch (error) {
-			console.log(error);
+		catch(e){
+			console.log(e);
 		}
 	}
 
-	return (
-		<section className="bg-white dark:bg-gray-900">
-			<div className="container px-6 py-10 mx-auto">
-				<h1 className="w-[500px] mx-auto text-center text-6xl">Fun facts app</h1>
-				<p className="w-[1000px] mx-auto text-center mt-4 text-3xl">This is an app that showcases fun facts</p>
+	console.log(records);
 
-				<div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
-					{records.map(record => (
-						<div
-							key={record._id}
-							className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-							<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-								{record.title}
-							</h5>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								{record.description}
-							</p>
-							<div className={"flex justify-center mt-4"}>
+	return (
+		<section className={"bg-white"}>
+			<div className={"container px-6 py-10 mx-auto"}>
+				<h1 className={"w-[500px] mx-auto text-center text-6xl font-bold text-blue-600"}>Fun facts app</h1>
+				<p className={"w-[1000px] mx-auto text-center mt-4 text-3xl text-blue-600"}>This is an app that showcases fun facts</p>
+
+				<div className={"grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3"}>
+					{
+						records.map(record => (
+							<div key={record._id}
+								className={"max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow "}
+							>
+								<h3 className={"mb-2 text-2xl font-bold text-red-500"}>{record.title}</h3>
+								<p className={"font-normal "}>{record.description}</p>
+
 								<button type="button"
-								        id={record._id}
 								        onClick={deleteRecord}
-								        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete
+								        id={record._id}
+								        className="mt-4 text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+									Delete!
 								</button>
 							</div>
-						</div>
-					))}
+						))
+					}
 				</div>
 			</div>
 		</section>
